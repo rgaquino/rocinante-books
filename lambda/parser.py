f6b9278de1b20fd341095477dbc79067e05cff9a
RG Aquino <ralphgregoraquino@gmail.com>
2020-03-29 18:00:08 +0800

    Add manual transcription parser

--- lambda/parser.py
+++ lambda/parser.py
@@ -1,21 +1,24 @@
 import csv
 import json
 import re
+import sys
 from datetime import datetime
 
 INVALID_FINAL_CHARS = [",", ":", ";"]
 
 
 def format_title(title: str) -> (str, str):
-    colon = row[0].find(":")
-    if colon:
+    colon = title.find(":")
+    if colon >= 0:
         sub = title.title()[colon+1:].strip()
+    else:
+        sub = ""
     title = title.title()[:colon].strip()
     return title, sub
 
 
 def format_author(author: str) -> str:
-    authors = author.split(",")
+    authors = author.strip().split(",")
     author = authors[0]
     if len(authors) == 1:
         return author
@@ -45,6 +48,10 @@ def format_highlight(highlight: str) -> str:
     return highlight
 
 
+def format_category(category: str) -> str:
+    return category.strip()
+
+
 def format_slug(title: str, author: str) -> str:
     title = re.sub(r'[^A-Za-z0-9 ]+', '', title)
     author = re.sub(r'[^A-Za-z0-9 ]+', '', author)
@@ -62,33 +69,77 @@ BOOK_FINISHED_AT = "finishedAt"
 BOOK_LAST_FINISHED_AT = "lastFinishedAt"
 BOOK_CATEGORY = "category"
 
-now: str = datetime.now().isoformat()
-book: dict = {
-    BOOK_ID: 0,
-    BOOK_TITLE: "",
-    BOOK_AUTHOR: "",
-    BOOK_HIGHLIGHTS: [],
-    BOOK_SLUG: "",
-    BOOK_FINISHED_AT: [now],
-    BOOK_LAST_FINISHED_AT: now,
-    BOOK_CATEGORY: ""
-}
 
-with open('sample_in.csv', 'r') as file:
-    reader = csv.reader(file, delimiter=',')
-    count = 0
-    for row in reader:
-        if count == 1:
-            book[BOOK_TITLE], book[BOOK_SUBTITLE] = format_title(row[0])
-        elif count == 2:
-            book[BOOK_AUTHOR] = format_author(row[0][3:])
-        elif count < 8:
+def new_book() -> dict:
+    now: str = datetime.now().isoformat()
+    return {
+        BOOK_ID: 0,
+        BOOK_TITLE: "",
+        BOOK_AUTHOR: "",
+        BOOK_HIGHLIGHTS: [],
+        BOOK_SLUG: "",
+        BOOK_FINISHED_AT: [now],
+        BOOK_LAST_FINISHED_AT: now,
+        BOOK_CATEGORY: ""
+    }
+
+
+def parse_kindle(fn):
+    book = new_book()
+    with open(fn, 'r') as file:
+        reader = csv.reader(file, delimiter=',')
+        count = 0
+        for row in reader:
+            if count == 1:
+                book[BOOK_TITLE],  sub = format_title(row[0])
+                if sub:
+                    book[BOOK_SUBTITLE] = sub
+            elif count == 2:
+                book[BOOK_AUTHOR] = format_author(row[0][3:])
+            elif count < 8:
+                count += 1
+                continue
+            else:
+                highlight = format_highlight(row[3])
+                book[BOOK_HIGHLIGHTS].append(highlight)
             count += 1
-            continue
-        else:
-            highlight = format_highlight(row[3])
-            book[BOOK_HIGHLIGHTS].append(highlight)
-        count += 1
 
+        book[BOOK_SLUG] = format_slug(book[BOOK_TITLE], book[BOOK_AUTHOR])
+        print(json.dumps(book, indent=4))
+
+
+def parse_manual(fn):
+    book = new_book()
+    file = open(fn, 'r')
+    count = 0
+    for line in file:
+        if count == 0:
+            book[BOOK_TITLE],  sub = format_title(line)
+            if sub:
+                book[BOOK_SUBTITLE] = sub
+        elif count == 1:
+            book[BOOK_AUTHOR] = format_author(line)
+        elif count == 2:
+            book[BOOK_CATEGORY] = format_category(line)
+        else:
+            if line.strip():
+                highlight = format_highlight(line)
+                book[BOOK_HIGHLIGHTS].append(highlight)
+        count += 1
     book[BOOK_SLUG] = format_slug(book[BOOK_TITLE], book[BOOK_AUTHOR])
     print(json.dumps(book, indent=4))
+
+
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: p3 parser.py <source_type> <file_name>")
+    elif sys.argv[1] == 'kindle':
+        parse_kindle(sys.argv[2])
+    elif sys.argv[1] == 'manual':
+        parse_manual(sys.argv[2])
+    else:
+        print("<source_type> can only be either 'kindle' or 'manual'")
+
+
+if __name__ == "__main__":
+    main()
